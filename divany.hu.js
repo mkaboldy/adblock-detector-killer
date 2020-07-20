a06ca8253f41eb9874ea174b2e2b0102f672a2ec
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-07-20 17:34:32 +0200

    divany.hu added

---
+++ divany.hu.js
@@ -0,0 +1,3 @@
+jQuery('section').has('a[href="https://divany.hu/adblock"]').remove();
+jQuery('body').css('overflow','auto');
+jQuery('div').has('a[href="https://divany.hu/adatvedelem/"]').remove();
