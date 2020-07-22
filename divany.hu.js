3ba8848adf161903e299be9ecbe8d01aea0ebada
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-07-22 20:31:18 +0200

    a useless attempt

--- divany.hu.js
+++ divany.hu.js
@@ -1,3 +1,9 @@
-jQuery('section').has('a[href="https://divany.hu/adblock"]').remove();
-jQuery('body').css('overflow','auto');
-jQuery('div').has('a[href="https://divany.hu/adatvedelem/"]').remove();
+// this doesn't work:(
+jQuery(document).ready(function(){
+    setInterval(function(){
+        console.log('divany.hu.js');
+        jQuery('section').has('a[href="https://divany.hu/adblock"]').remove();
+        jQuery('body').css('overflow','auto');
+        jQuery('div').has('a[href="https://divany.hu/adatvedelem/"]').remove();    
+    }, 1000);
+});
